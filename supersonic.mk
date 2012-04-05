bc0d4d8d15fa0fe17a9f9a6fae9591752ce20a1b
TwistedUmbrella <twistedumbrella@gmail.com>
2012-04-05 15:57:50 -0400

    Revert "Instantaneous GPS Fixation v1.2 [AngelDeath]"
    This reverts commit 74077532ec37182b7d8a346bf8901024a8076b85.

--- supersonic.mk
+++ supersonic.mk
@@ -19,9 +19,9 @@
 # not specialized for any geography.
 #
 
-# Device GPS Properties
+# The gps config appropriate for this device
 PRODUCT_COPY_FILES += \
-    device/htc/common/system/etc/gps.conf:system/etc/gps.conf
+    device/htc/supersonic/prebuilt/etc/gps.conf:system/etc/gps.conf
 
 ## (1) First, the most specific values, i.e. the aspects that are specific to GSM
 PRODUCT_COPY_FILES += \
